bb56b3f0493ee97c968fdfc0005d034113bea0c3
elvismaicol <elvismaicol@gmail.com>
2020-02-21 23:41:54 -0300

    Transações 3 - Rollback

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -16,25 +17,32 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			
-			//conn.setAutoCommit(false);
+			conn.setAutoCommit(false);
 			
 			st = conn.createStatement();
 			
 			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
-			int x = 1;
+			/*int x = 1;
 			
 			if (x < 2) {
 				throw new SQLException("Fake error");
-			}
+			}*/
 			
 			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-			  
+			
+			conn.commit();
+			
 			System.out.println("rows1 " + rows1);
 			System.out.println("rows2 " + rows2);
 		}	
 		catch (SQLException e) {
-			e.printStackTrace();
+			try {
+				conn.rollback();
+				throw new DbException("Transaction roller back! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Cause by: " + e.getMessage());
+			}
 		}
 		finally {
 			DB.closeStatement(st);
